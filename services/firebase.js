d846331f831817fc41bca6ebf5dad68def2e6d61
alvinlucas <84456349+alvinlucas@users.noreply.github.com>
2025-08-13 17:30:13 +0200

    feat: modif appel a variable secret

--- services/firebase.js
+++ services/firebase.js
@@ -1,6 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
-
+if (process.env.FIREBASE_KEY_JSON) {
+    // En CI GitHub : on lit la clé JSON à partir de la variable d'environnement
+    serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
+} else {
+    // En local : on lit le fichier
+    serviceAccount = require("../serviceAccountKey.json");
+}
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
